cff432d286e70b781e5c4aeb91716156950c5574
Eli Vickery <e.n.vickery@gmail.com>
2017-02-13 15:34:26 -0800

    Updated messages

--- rps.py
+++ rps.py
@@ -2,14 +2,14 @@
 
 #import random and time modules for later use
 import random
-# import time
-# import sys
+import time
+import sys
 
-# def print_slow(str):
-#     for letter in str:
-#         sys.stdout.write(letter)
-#         sys.stdout.flush()
-#         time.sleep(0.3)
+def print_slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.2)
 
 def get_user_choice():
 	user_choice = str(raw_input('Welcome to Rock Paper Scissors!\nMake a choice: rock (R), paper (P) or scissors (S). ')).upper()
@@ -73,7 +73,11 @@ def play_again():
 		if(play_again_choice == 'Y'):
 			print run_game()
 		elif (play_again_choice == 'N'):
+			print 'Ok, thanks for playing!'
 			exit()
+		elif (play_again_choice in ("CONVINCE ME","NOT SURE","I DON'T KNOW")):
+			convince_messages = ("Rock Paper Scissors is the best game ever! Don't believe me? Try it yourself.","C'mon, let's play a game!","You should definitely type Y.")
+			print random.choice(convince_messages)
 		else:
 			print "I didn't understand that."
 	
@@ -87,6 +91,8 @@ def run_game():
 		computer_choice = get_computer_choice()
 
 		beats_result = get_beats_list(user_choice, computer_choice)
+
+		print_slow('\n...\n...\n...\n')
 
 		return "Your choice was %s.\n\nThe computer's choice was %s.\n\n%s\n" % (user_choice,computer_choice,beats_result)
 
